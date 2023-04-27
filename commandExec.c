38ce2de4e37dfc74e0d73adc409803a7d5b2292e
Mario-Mellek <mario.samy266@gmail.com>
2023-04-27 03:49:16 +0200

    Added a fork faluire checker

--- commandExec.c
+++ commandExec.c
@@ -12,12 +12,19 @@ void commandExec(char *args[], char *program_name)
 
 	/*Forks a new process*/
 	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+
 	if (pid == 0)
 	{
 		/*Child process executes the command*/
 		execve(args[0], args, environ);
 		perror(program_name);
-		exit(0);
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
