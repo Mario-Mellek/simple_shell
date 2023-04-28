27f4ebddb781a214b0333aa8082aae59996fa266
Mario-Mellek <mario.samy266@gmail.com>
2023-04-28 22:32:25 +0200

    Fixing main.c

--- main.c
+++ main.c
@@ -26,7 +26,6 @@ int main(int __attribute__((unused)) argc, char **argv)
 		read = getline(&line, &len, stdin);
 		if (read == -1)
 		{
-			/*putchar('\n');*/
 			break;
 		}
 		token = strtok(line, " ");
