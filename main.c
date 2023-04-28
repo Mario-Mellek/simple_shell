61cb726bc3f3ea56c43212243f0630b2c039a89e
Mario-Mellek <mario.samy266@gmail.com>
2023-04-28 12:42:17 +0200

    moified main to not display the prompt in non-interactive mode

--- main.c
+++ main.c
@@ -12,14 +12,16 @@ int main(int __attribute__((unused)) argc, char **argv)
 	char *token, *line = NULL;
 	char *args[MAX_COMMAND / 2 + 1];
 	ssize_t read;
+	int interactive = isatty(STDIN_FILENO);
 
 	while (1)
 	{
 		j = 0;
-		/*The Prompt being flushed to appear on every submission*/
+		if (interactive)
+		{/*The Prompt being flushed to appear on every submission*/
 		printf("\033[32mSimple-Shell$\033[0m ");
 		fflush(stdout);
-
+		}
 		/*Reads user input*/
 		read = getline(&line, &len, stdin);
 		if (read == -1)
@@ -35,8 +37,7 @@ int main(int __attribute__((unused)) argc, char **argv)
 		}
 		args[j] = NULL;
 		line[read - 1] = '\0';
-		/*Sets the 1st argument to input, NULL terminated*/
-		if (j > 0)
+		if (j > 0) /*Sets the 1st argument to input, NULL terminated*/
 			args[0] = args[0];
 		else
 			args[0] = line;
