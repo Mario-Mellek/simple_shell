87f5b7d96d792213b6c11cb863eaa2b36a22a178
Mario-Mellek <mario.samy266@gmail.com>
2023-04-29 03:11:39 +0200

    Handling leading and trailing whitespaces

--- Simple-Shell_practice/pathTest/main.c
+++ Simple-Shell_practice/pathTest/main.c
@@ -12,19 +12,20 @@ int main(int __attribute__((unused)) argc, char **argv)
 	char *token, *line = NULL;
 	char *args[MAX_COMMAND / 2 + 1];
 	ssize_t read;
+	int interactive = isatty(STDIN_FILENO);
 
 	while (1)
 	{
 		j = 0;
-		/*The Prompt being flushed to appear on every submission*/
+		if (interactive)
+		{/*The Prompt being flushed to appear on every submission*/
 		printf("\033[32mSimple-Shell$\033[0m ");
 		fflush(stdout);
-
+		}
 		/*Reads user input*/
 		read = getline(&line, &len, stdin);
 		if (read == -1)
 		{
-			putchar('\n');
 			break;
 		}
 		token = strtok(line, " ");
@@ -35,8 +36,7 @@ int main(int __attribute__((unused)) argc, char **argv)
 		}
 		args[j] = NULL;
 		line[read - 1] = '\0';
-		/*Sets the 1st argument to input, NULL terminated*/
-		if (j > 0)
+		if (j > 0) /*Sets the 1st argument to input, NULL terminated*/
 			args[0] = args[0];
 		else
 			args[0] = line;
